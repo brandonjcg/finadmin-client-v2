a1ecc336af0848c1be4625739621014aa7f13e35
Brandon Castillo <brajoel98@gmail.com>
2025-01-01 23:07:31 -0800

    feat(pages): :sparkles: add filters in url for api

--- src/components/Checkboxes/CheckboxFlag.tsx
+++ src/components/Checkboxes/CheckboxFlag.tsx
@@ -1,8 +1,16 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { TCheckboxFlags } from '@/types';
 import { CheckboxFlagItem } from './CheckboxFlagItem';
+import { PREFIX_FILTERS } from '@/constants';
+
+const mapFlags: { [key in TCheckboxFlags]: TCheckboxFlags } = {
+  checked: 'indeterminate',
+  unchecked: 'checked',
+  indeterminate: 'unchecked',
+};
 
 interface Props {
   name: string;
@@ -15,18 +23,40 @@ export const CheckboxFlag = ({
   label,
   initialValue = 'unchecked',
 }: Props) => {
-  const [checked, setChecked] = useState<TCheckboxFlags>(initialValue);
+  const router = useRouter();
+  const params = useSearchParams();
+  const [checked, setChecked] = useState<TCheckboxFlags>(() => {
+    const valueFromParams = params.get(`${PREFIX_FILTERS}[${name}]`);
+    if (valueFromParams === 'true') return 'checked';
+    if (valueFromParams === 'false') return 'indeterminate';
+    return initialValue;
+  });
 
-  console.log(
-    `🚀 ${new Date().toLocaleString('en-US', { timeZone: 'America/Tijuana', hour12: false })} ~ ${label} ~ checked:`,
-    checked,
-  );
+  useEffect(() => {
+    const valueFromParams = params.get(`${PREFIX_FILTERS}[${name}]`);
+    let newState: TCheckboxFlags = initialValue;
+    if (valueFromParams === 'true') newState = 'checked';
+    if (valueFromParams === 'false') newState = 'indeterminate';
+    if (newState !== checked) {
+      setChecked(newState);
+    }
+  }, [name, params, initialValue, checked]);
+
   const handleChange = () => {
-    setChecked((prev) => {
-      if (prev === 'checked') return 'unchecked';
-      if (prev === 'unchecked') return 'indeterminate';
-      return 'checked';
-    });
+    const newState = mapFlags[checked];
+    const url = new URL(window.location.href);
+
+    if (newState === 'unchecked') {
+      url.searchParams.delete(`${PREFIX_FILTERS}[${name}]`);
+    } else {
+      url.searchParams.set(
+        `${PREFIX_FILTERS}[${name}]`,
+        newState === 'checked' ? 'true' : 'false',
+      );
+    }
+
+    setChecked(newState);
+    router.push(url.toString());
   };
 
   return (
