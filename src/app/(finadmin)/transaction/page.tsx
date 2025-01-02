b3197191e49a78ac7732d3bf8c025093d3258be1
Brandon Castillo <brajoel98@gmail.com>
2025-01-01 23:25:59 -0800

    feat(pages): :sparkles: add button of add transaction

--- src/app/(finadmin)/transaction/page.tsx
+++ src/app/(finadmin)/transaction/page.tsx
@@ -1,4 +1,5 @@
 import { ColumnDef } from '@tanstack/react-table';
+import Link from 'next/link';
 import { CheckboxFlag, DataTable, SelectBanks } from '@/components';
 import { ITransaction } from '@/interfaces';
 import { PREFIX_FILTERS } from '@/constants';
@@ -35,9 +36,18 @@ const columns: ColumnDef<ITransaction>[] = [
 ];
 
 export default function TransactionPage() {
+  const name = 'transaction';
   return (
     <>
-      <h1 className="text-2xl font-bold mb-5">Transactions</h1>
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-bold">Transactions</h1>
+        <Link
+          href={`/${name}/new`}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+        >
+          Add transaction
+        </Link>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
         <SelectBanks
           label="Bank"
@@ -47,7 +57,7 @@ export default function TransactionPage() {
         <CheckboxFlag name="isPaid" label="Is paid?" />
         <CheckboxFlag name="isReserved" label="Is reserved?" />
       </div>
-      <DataTable columns={columns} endpoint={'transaction'} />
+      <DataTable columns={columns} endpoint={name} />
     </>
   );
 }
